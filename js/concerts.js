6c52bf2f7a8d4d0abff484a65d06bf5b5c6ad9cd
trudzer <trudzeredits@gmail.com>
2019-05-06 15:21:06 +0000

    Add files via upload

---
+++ js/concerts.js
@@ -0,0 +1,162 @@
+let concertsjs = {
+	"results":
+	[
+		{
+			"eventDateName":"Dimma í Bæjarbíói",
+			"name":"Aukatónleikar",
+			"dateOfShow":"2019-05-02T20:30:00",
+			"userGroupName":"Bæjarbíó slf",
+			"eventHallName":"Bæjarbíó (Hafnarfirði)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10840.jpg"
+		},
+		{
+			"eventDateName":"Dimma í Bæjarbíói",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-03T20:30:00",
+			"userGroupName":"Bæjarbíó slf",
+			"eventHallName":"Bæjarbíó (Hafnarfirði)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10840.jpg"
+		},
+		{
+			"eventDateName":"HNÍSAN x YUNG NIGO DRIPPIN x 24/7 x DANIIL",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-04T23:59:00",
+			"userGroupName":"Agent.is","eventHallName":"H30 Club",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10938.jpg"},
+		{
+			"eventDateName":"Svavar Knútur í Bæjarbíói",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-09T20:30:00",
+			"userGroupName":"Bæjarbíó slf",
+			"eventHallName":"Bæjarbíó (Hafnarfirði)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10763.jpg"
+		},
+			{
+			"eventDateName":"Vortónleikar Guðrúnar Gunnars",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-10T20:00:00",
+			"userGroupName":"GG",
+			"eventHallName":"Fríkirkjan í Reykjavík",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10898.jpg"
+		},
+		{	"eventDateName":"Boð til Belgrad // Evening in Belgrade",
+			"name":"Boð til Belgrad // Evening in Belgrade",
+			"dateOfShow":"2019-05-10T20:00:00",
+			"userGroupName":"Hönnunarsafn Íslands",
+			"eventHallName":"Hönnunarsafn Íslands",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10942.jpg"
+		},
+			{
+			"eventDateName":"Baggalútur - Leysir frá skjóðunni í Bæjarbíói",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-10T20:30:00",
+			"userGroupName":"Bæjarbíó slf",
+			"eventHallName":"Bæjarbíó (Hafnarfirði)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10914.jpg"
+		},
+		{
+			"eventDateName":"Dúndurfréttir á Akranesi",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-10T21:30:00",
+			"userGroupName":"Veislur & Viðburðir ehf.",
+			"eventHallName":"Gamla Kaupfélagið (Akranesi)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10941.jpg"
+		},
+			{
+			"eventDateName":"Mótettukórinn í Akureyrarkirkju",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-11T17:00:00",
+			"userGroupName":"Mótettukór Hallgrímskirkju",
+			"eventHallName":"Akureyrarkirkja",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10899.jpg"
+		},
+			{
+			"eventDateName":"Æfing 50 ára - Lokadagstónleikar í Bæjarbíói",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-11T20:30:00",
+			"userGroupName":"Bæjarbíó slf",
+			"eventHallName":"Bæjarbíó (Hafnarfirði)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10858.jpg"
+		},
+			{
+			"eventDateName":"Papaball í Hlégarði - Vordansleikur",
+			"name":"Dansleikur",
+			"dateOfShow":"2019-05-11T23:59:00",
+			"userGroupName":"Bæjarbíó slf",
+			"eventHallName":"Hlégarður",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10891.jpg"
+		},
+			{
+			"eventDateName":"Albatross - Fyrsti dansleikurinn í Alþýðuhúsinu",
+			"name":"Dansleikur",
+			"dateOfShow":"2019-05-18T23:00:00",
+			"userGroupName":"Alþýðuhúsið Menningarfélag",
+			"eventHallName":"Alþýðuhúsið Vestmannaeyjum",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10890.jpg"
+		},
+			{
+			"eventDateName":"Kristín Stefánsdóttir syngur lög Noruh Jones í Bæjarbíói",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-23T20:30:00",
+			"userGroupName":"Bæjarbíó slf",
+			"eventHallName":"Bæjarbíó (Hafnarfirði)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10831.jpg"
+		},
+			{
+			"eventDateName":"ADHD Útgáfutónleikar",
+			"name":"Græni Hatturinn",
+			"dateOfShow":"2019-05-23T21:00:00",
+			"userGroupName":"Blómi sf.",
+			"eventHallName":"Græni Hatturinn (Akureyri)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10923.jpg"
+		},
+			{
+			"eventDateName":"Föstudagslögin með Stebba Jak og Andra Ívars í Bæjarbíói",
+			"name":"Tónleikar",
+			"dateOfShow":"2019-05-24T20:30:00",
+			"userGroupName":"Bæjarbíó slf",
+			"eventHallName":"Bæjarbíó (Hafnarfirði)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10936.jpg"
+		},
+			{
+			"eventDateName":"RADIOHEAD - ROKKMESSA",
+			"name":"Reykjavík",
+			"dateOfShow":"2019-05-24T22:00:00",
+			"userGroupName":"Spock ehf.",
+			"eventHallName":"Hard Rock Cafe",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10886.jpg"
+		},
+			{
+			"eventDateName":"RADIOHEAD - ROKKMESSA",
+			"name":"Akureyri",
+			"dateOfShow":"2019-05-25T22:00:00",
+			"userGroupName":"Spock ehf.",
+			"eventHallName":"Græni Hatturinn (Akureyri)",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10886.jpg"
+		},
+			{
+			"eventDateName":"Upphafstónleikar Kirkjulistahátíðar 2019",
+			"name":"Kirkjulistahátíð 2019",
+			"dateOfShow":"2019-06-01T17:00:00",
+			"userGroupName":"Kirkjulistahátíð",
+			"eventHallName":"Hallgrímskirkja",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10916.jpg"
+		},
+			{
+			"eventDateName":"Upphafstónleikar Kirkjulistahátíðar 2019",
+			"name":"Kirkjulistahátíð 2019",
+			"dateOfShow":"2019-06-02T17:00:00",
+			"userGroupName":"Kirkjulistahátíð",
+			"eventHallName":"Hallgrímskirkja",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10916.jpg"
+		},
+			{
+			"eventDateName":"Útlendingurinn",
+			"name":"Kirkjulistahátíð 2019",
+			"dateOfShow":"2019-06-03T21:00:00",
+			"userGroupName":"Kirkjulistahátíð",
+			"eventHallName":"Hallgrímskirkja",
+			"imageSource":"https://d30qys758zh01z.cloudfront.net/images/medium/1.10917.jpg"
+		}
+	]
+}
--- js/script.js
+++ js/script.js
@@ -1,30 +1,58 @@
-  function createNode(element) {
-      return document.createElement(element);
-  }
+function createNode(element) {
+  return document.createElement(element);
+}
 
-  function append(parent, el) {
-    return parent.appendChild(el);
-  }
-  const ul = document.getElementById('concerts');
-  const url = 'https://apis.is/concerts';
-  fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-    let concerts = data.results;
-    return concerts.map(function(concert) {
-      let li = createNode('li'),
-      	  img = createNode('img'),
-          span = createNode('span');
-      img.src = concert.imageSource;
-      moment.locale('is');
-      let date = moment(concert.dateOfShow).format('LLL');
-      concert.dateOfShow = date;
-      span.innerHTML = `${concert.eventDateName} ${concert.name} <br /> ${concert.dateOfShow} <br /> ${concert.userGroupName} ${concert.eventHallName}`;
-      append(li, img);
-      append(li, span);
-      append(ul, li);
-    })
-  })
-  .catch(function(error) {
-    console.log(JSON.stringify(error));
+function append(parent, el) {
+return parent.appendChild(el);
+}
+const ul = document.getElementById('concerts');
+const url = 'https://apis.is/concerts';
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+let concerts = data.results;
+	return concerts.map(function(concert) {
+	  let li = createNode('li'),
+	  	  img = createNode('img'),
+	      span = createNode('span');
+	  img.src = concert.imageSource;
+	  moment.locale('is');
+	  let date = moment(concert.dateOfShow).format('LLL');
+	  concert.dateOfShow = date;
+	  span.innerHTML = `${concert.eventDateName} ${concert.name} <br /> ${concert.dateOfShow} <br /> ${concert.userGroupName} ${concert.eventHallName}`;
+	  append(li, img);
+	  append(li, span);
+	  append(ul, li);
+	})
+})
+.catch(function(error) {
+console.log(JSON.stringify(error));
+});
+
+function searchFunction() {
+	let input, filter, ul, li, a, i, txtValue;
+
+	input = document.getElementById('search');
+	filter = input.value.toUpperCase();
+	ul = document.getElementById('concerts');
+	li = document.getElementsByTagName('li');
+	
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName('span')[0];
+		txtValue = a.textContent || a.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+  	}
+};
+
+$('input[name="dates"]').daterangepicker();
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
   });
+});
